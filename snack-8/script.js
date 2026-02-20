7e5e51a93faf9f2491ae52f05d54e4115139faa2
christian-matarazzo <christian.matarazzo95@gmail.com>
2026-02-20 15:31:14 +0100

    eighth snack

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,31 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+
+
+
+//for method
+
+/* let findUserClass = []
+for (let i = 0; i < students.length; i++) {
+  const student = students[i];
+
+  if(student.name === "Marco Lanci") 
+  {
+    findUserClass.push(student.class)
+  }
+  
+}
+console.log(findUserClass); */
+
+
+
+//find method
+const findUserClass = students.find(function(user){
+if(user.name === 'Marco Lanci') {
+  return user
+}
+})
+
+console.log(findUserClass.class);
