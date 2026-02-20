28c747a39e29e50af7f7a7c053e8a165f611033b
christian-matarazzo <christian.matarazzo95@gmail.com>
2026-02-20 14:40:46 +0100

    third snack

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,36 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+
+
+
+
+/* let sumNumbers = []
+
+for (let i = 0; i < numbers.length; i++) {
+    const number = numbers[i];
+
+    sum = number + 1
+
+
+    sumNumbers.push(sum)
+    
+}
+
+console.log(sumNumbers); */
+
+
+
+
+
+
+
+
+ const sumNumber= numbers.map (function(number) {
+    number = number + 1 
+
+    return number
+    
+})
+console.log(sumNumber); 
